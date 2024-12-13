571c6a5d48609ef3e5e58b37ca591df774502bee
freevue <hackish113@gmail.com>
2024-12-13 16:45:31 +0900

    Update og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -7,7 +7,7 @@ export const config = {
 const font = fetch(new URL("../../assets/BMDOHYEON.ttf", import.meta.url)).then(
   (res) => res.arrayBuffer()
 );
-const [primary, secodary] = GET_COLOR[new Date().getFullYear()];
+const [primary, secodary] = GET_COLOR(new Date().getFullYear());
 const primaryRGB = `rgb(${primary.join(",")})`;
 const SECONDARY_RGB = `rgb(${(secodary || SECONDARY_COLOR(primary)).join(
   ","
